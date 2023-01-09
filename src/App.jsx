b87c1bbbee0e9d5ec43ef4cd4761d355de2aeb33
nahomik <nahomgore@gmail.com>
2023-01-10 02:23:06 +0300

    Fixed bugs

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// App.jsx - Main application component for AuthenChain
+// Handles game logic, state management, and UI composition
+
+import { useState, useEffect } from "react";
+import "./App.css";
+import CardGrid from "./components/CardGrid";
+import Scoreboard from "./components/Scoreboard";
+import Modal from "./components/Modal";
+import logo from "./assets/ghibli-films/ghibli.svg";
+import loadingImage from "./assets/ghibli-films/studio-ghibli.gif";
+import customImages from "./components/CustomImages";
+import shuffleArray from "./utils/shuffleArray";
+import formatTime from "./utils/formatTime";
+import useGameTimer from "./hooks/useGameTimer";
+import { GHIBLI_API_URL } from "./constants/api";
+
+function App() {
+  // ...existing code...
+}
+
+export default App;
